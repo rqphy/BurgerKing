141635991a46233626b1c26652d5eea4d575b903
Raphael <raphael.ferreira@hetic.net>
2022-08-17 20:47:46 +0200

    :bug: app > call undefined component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { useEffect, useState, useRef } from 'react'
 import { RecoilRoot } from 'recoil'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
-import { Nav } from '@components'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
